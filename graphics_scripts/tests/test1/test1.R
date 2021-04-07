6fa4fe25923a5d4818ea576c4c25d95b146b1dbf
Nathaniel <njfernandes24@tamu.edu>
2021-04-07 13:13:37 -0500

    cleaned up and reran test 1 (using all samples to crossvalidate and find best short/long ma pair).
    Note: need to fix external algorithms calculations after adding 3 more samples.

--- graphics_scripts/tests/test1/test1.R
+++ graphics_scripts/tests/test1/test1.R
@@ -1,20 +1,32 @@
 # This file contains a baseline test. All 49 samples are used to cross validate and select the best long and short moving average
 
-# 1. Find best short/long moving average pair
+# 1. Do 6-fold cross-validation to estimate algorithm accuracy on unseen data
+ps <- pod_split(1:length(cgm_dataset_df), 6, seed = 200)
+load('./graphics_scripts/tests/test1/cv_errors.Rda')
+#cv_errors <- cross_val(ps, vector = TRUE) # expensive
+mean(cv_errors) # 10.03% 
 
-pem_all <- create_pem2(1:46)
-save(pem_all, file="./graphics_scripts/tests/test3/pem_all.Rda")
+#save(cv_errors, file="./graphics_scripts/tests/test1/cv_errors.Rda")
+
+# 2. Find best short/long moving average pair
+
 load('./graphics_scripts/tests/test1/pem_all.Rda')
-plot_heatmap(pem_all)
-View(find_min_poderror(pem_all))
+# pem_all <- create_pem2(1:length(cgm_dataset_df)) # expensive
+#save(pem_all, file="./graphics_scripts/tests/test1/pem_all.Rda")
 
-errors_df <- make_boxplot_df(1:46, short_ma = 5, long_ma=32)
+plot_heatmap(pem_all) # Can visualize results
+best_ma <- find_min_poderror(pem_all)[1,]
+
+# Note: need to fix calculation of other algorithms now that added 3 more samples
+errors_df <- make_boxplot_df(1:length(cgm_dataset_df), short_ma=best_ma$short, long_ma=best_ma$long)
 plot_boxplot(errors_df, "Algorithm Comparison vs Manual (5, 32)")
 
 # data analysis
-sd_err <- sapply(errors_df, sd, na.rm=TRUE)
-summary_err <- sapply(errors_df, summary)
-View(summary_err)
+#sd_err <- sapply(errors_df, sd, na.rm=TRUE)
+#summary_err <- sapply(errors_df, summary)
+#View(summary_err)
+
+
 # save(cross_validation_error, file="./graphics_scripts/tests/test1/cross_validation_error.Rda")
 # save(o_1, file="./graphics_scripts/tests/test1/optimize.Rda")
 # save(errors_df, file="./graphics_scripts/tests/test1/errors.Rda")
