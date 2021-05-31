fe0781f2c0822c183c7d492583718de89ad85f60
Nathaniel <njfernandes24@tamu.edu>
2021-05-31 16:28:19 -0500

    Updated cgmanalysis after adding new data. Caught major bug.
    BUG - cgmvariables was returning the data in the order of the files it read in, which wasn't the actual order due to how R sorts files (i.e. test1.csv, test10.csv, test2.csv).
    
    This was not corrected/resorted when did the data analysis which resulted in the very high errors seen for CGMAnalysis.

--- graphics_scripts/external algorithms/cgmanalysis_manual_calc.R
+++ graphics_scripts/external algorithms/cgmanalysis_manual_calc.R
@@ -2,12 +2,11 @@
 if(!require(cgmanalysis)) install.packages('cgmanalysis')
 library(cgmanalysis)
 
-#cgmvariables("./graphics_scripts/external algorithms/data/files/", "./graphics_scripts/external algorithms/data/", outputname = "cgmanalysis data") # test43.csv throws an error sometimes, so delete it and rerun if that is the case
+#cgmvariables("./graphics_scripts/external algorithms/data/files/", "./graphics_scripts/external algorithms/data/", outputname = "cgmanalysis data")
 
 cgmanalysis_data <- read.csv('./graphics_scripts/external algorithms/data/cgmanalysis data.csv')
 
 cgmanalysis_mage <- cgmanalysis_data$r_mage
-cgmanalysis_mage_all <- c(cgmanalysis_mage[1:42], NaN, cgmanalysis_mage[43:length(cgmanalysis_mage)]) # insert NaN in index 43 because it throws an error
 
 # Old cgmanalysis data
 cgmanalysis_data_old <- read.csv('./graphics_scripts/external algorithms/data/cgmanalysis data (v2.7).csv')
