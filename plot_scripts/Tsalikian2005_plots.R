ccd2cf5046b7ab59ebd1e61f453e848f4bd75e94
LChun <lizzie_chun1@tamu.edu>
2021-07-20 14:13:26 -0500

    update plot_scripts and plots to final 45 traces

--- plot_scripts/Tsalikian2005_plots.R
+++ plot_scripts/Tsalikian2005_plots.R
@@ -1,6 +1,6 @@
-# Script to reproduce plots for Tsalikian (2005) subjects 15, 11, 8, and 44 manual MAGE
+# Script to reproduce plots for Tsalikian (2005) manual MAGE
 # Author: Elizabeth Chun
-# Date: February 2nd, 2021
+# Date: February 2nd, 2021; edited July 20th, 2021
 
 # This dataset comes from a publically available CGM dataset. Both the link to the original
 # download site and a preprocessor script can be found through our Awesome-CGM GitHub
@@ -20,23 +20,35 @@ library(ggplot2)
 
 # Subset the data exactly as was manually done
 plot_data <- list()
-plot_data[[1]] <- datafile[1601:1755, ]
-plot_data[[2]] <- datafile[3076:3230, ]
-plot_data[[3]] <- datafile[3319:3475, ]
-plot_data[[4]] <- datafile[3549:3701, ]
-plot_data[[5]] <- datafile[4272:4423, ]
+plot_data[[1]] <- datafile[1:135, ]
+plot_data[[2]] <- datafile[136:242, ]
+plot_data[[3]] <- datafile[243:384, ]
+plot_data[[4]] <- datafile[456:561, ]
+plot_data[[5]] <- datafile[1601:1755, ]
+plot_data[[6]] <- datafile[3076:3230, ]
+plot_data[[7]] <- datafile[3319:3475, ]
+plot_data[[8]] <- datafile[3549:3701, ]
+plot_data[[9]] <- datafile[4272:4423, ]
 
 # Name dataset, dates, and manual mage values 
-dataset <- "Tsalikian2005_subject15_11_8_44"
-dates <- c("1-30-2000", "3-3-2000", "3-19-2000", "3-18-2000", "3-6-2000")
-manual_mage <- c(68.5, 87.33, 360, 105.33, 283)
+dataset <- "Tsalikian2005"
+
+subjects = c()
+dates <- rep(as.Date(plot_data[[1]]$time[1]), length(plot_data))
+for (i in 1:length(plot_data)) {
+  subjects[i] <- unique(plot_data[[i]]$id)
+  dates[i] <- as.Date(plot_data[[i]]$time[1])
+}
+
+manual_mage <- c(138, 164, 135, 45, 55, 87, 360, 105, 283)
 
 # Save plots to pdf
 figure.path <- "plot_scripts/plots/"
 pdf(file = paste0(figure.path, dataset, ".pdf", sep = ""),
     width = 10, height = 5, onefile = TRUE)
 for (i in 1:length(plot_data)) {
-  p = plot_glu(plot_data[[i]]) + ggtitle(paste0("Manual MAGE: ", manual_mage[i], " (",
+  p = plot_glu(plot_data[[i]]) + ggtitle(paste0("Manual MAGE: ", manual_mage[i], 
+                                                " (", subjects[i], ", ",
                                                 dates[i], ")", sep = ""))
   print(p)
 }
